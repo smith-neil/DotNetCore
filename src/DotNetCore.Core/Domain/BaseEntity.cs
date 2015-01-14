1f7e0160498c9a860fcdaed8765a7b731bc4f9e1
Neil Smith <nsmith@envoc.com>
2015-01-13 20:04:33 -0600

    feat(core.domain): create the base entity

---
+++ src/DotNetCore.Core/Domain/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNetCore.Core.Domain
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime? DateLastModified { get; set; }
+    }
+}
