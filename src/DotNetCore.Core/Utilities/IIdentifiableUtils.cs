ecde13fc4809ecc2b4e5b3cf2746ad5df2e11781
Neil Smith <nsmith@envoc.com>
2015-01-13 20:27:07 -0600

    feat(core.utilities): create iidentifiable is new extension

---
+++ src/DotNetCore.Core/Utilities/IIdentifiableUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetCore.Core.Utilities
+{
+    public static class IIdentifiableUtils
+    {
+        public static bool IsNew<T>(this IIdentifiable<T> source)
+        {
+            source.ThrowIfNull("source");
+
+            return Equals(source.Id, default(T));
+        }
+    }
+}
