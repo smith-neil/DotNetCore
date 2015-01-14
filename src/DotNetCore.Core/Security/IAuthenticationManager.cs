297ab01d4187def2ae802b3f8783a3259a6ea50e
Neil Smith <nsmith@envoc.com>
2015-01-13 20:42:06 -0600

    feat(core.security): create auth manager interfaces

---
+++ src/DotNetCore.Core/Security/IAuthenticationManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DotNetCore.Core.Security.Models;
+
+namespace DotNetCore.Core.Security
+{
+    public interface IAuthenticationManager
+    {
+        IEnumerable<AuthenticationDescription> GetExternalAuthenticationTypes();
+
+        ExternalLoginInfo GetExternalLoginInfo();
+        Task<ExternalLoginInfo> GetExternalLoginInfoAsync();
+
+        void SignIn(params ClaimsIdentity[] identities);
+
+        void SignOut(params string[] authenticationTypes);
+    }
+}
