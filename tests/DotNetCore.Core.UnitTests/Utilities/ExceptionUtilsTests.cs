98e33b6a18041f454342fef1f82044e8de390589
Neil Smith <nsmith@envoc.com>
2015-01-13 19:32:35 -0600

    feat(core.utilities): create throw if null exception extension

---
+++ tests/DotNetCore.Core.UnitTests/Utilities/ExceptionUtilsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DotNetCore.Core.Utilities;
+using NUnit.Framework;
+
+namespace DotNetCore.Core.UnitTests.Utilities
+{
+    [TestFixture]
+    public class ExceptionUtilsTests
+    {
+        [TestCase(null, null)]
+        [TestCase("paramName", "message")]
+        public void ThrowIfNull_ThrowsNull(string paramName, string message)
+        {
+            string nullThing = null;
+
+            Assert.Throws<ArgumentNullException>(() => nullThing.ThrowIfNull(paramName, message));
+        }
+
+        [TestCase(null, null)]
+        [TestCase("paramName", "message")]
+        public void ThrowIfNull_DoesNotThrow(string paramName, string message)
+        {
+            string notNullThing = "im not null";
+
+            Assert.DoesNotThrow(() => notNullThing.ThrowIfNull(paramName, message));
+        }
+    }
+}
