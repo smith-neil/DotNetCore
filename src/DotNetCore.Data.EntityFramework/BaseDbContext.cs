099aa2ec0cbb6041d166cc322c8393a497c2b6f3
Neil Smith <nsmith@envoc.com>
2015-01-15 19:08:10 -0600

    feat(data.entityframework): create base context, repositories, and unit of work

---
+++ src/DotNetCore.Data.EntityFramework/BaseDbContext.cs
@@ -0,0 +1,119 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+using DotNetCore.Core.Domain;
+using DotNetCore.Core.Utilities;
+
+namespace DotNetCore.Data.EntityFramework
+{
+    public abstract class BaseDbContext : DbContext, IDbContext
+    {
+        private ObjectContext _objectContext;
+        private DbTransaction _transaction;
+
+        protected BaseDbContext() : base("AppContext")
+        {
+            
+        }
+
+        protected BaseDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+            
+        }
+
+        protected BaseDbContext(ILogger logger) : base("AppContext")
+        {
+            Database.Log = logger.Log;
+        }
+
+        protected BaseDbContext(string nameOrConnectionString, ILogger logger) : base(nameOrConnectionString)
+        {
+            Database.Log = logger.Log;
+        }
+
+        public new IDbSet<T> Set<T>() where T : BaseEntity
+        {
+            return base.Set<T>();
+        }
+
+        public void SetAsAdded<T>(T entity) where T : BaseEntity
+        {
+            UpdateEntityState(entity, EntityState.Added);
+        }
+
+        public void SetAsModified<T>(T entity) where T : BaseEntity
+        {
+            UpdateEntityState(entity, EntityState.Modified);
+        }
+
+        public void SetAsDeleted<T>(T entity) where T : BaseEntity
+        {
+            UpdateEntityState(entity, EntityState.Deleted);
+        }
+
+        public void BeginTransaction()
+        {
+            _objectContext = ((IObjectContextAdapter) this).ObjectContext;
+
+            if (_objectContext.Connection.State == ConnectionState.Open)
+                return;
+            
+            _objectContext.Connection.Open();
+            _transaction = _objectContext.Connection.BeginTransaction();
+        }
+
+        public int Commit()
+        {
+            var count  = SaveChanges();
+            _transaction.Commit();
+
+            return count;
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            var count = await SaveChangesAsync();
+            _transaction.Commit();
+
+            return count;
+        }
+
+        public void Rollback()
+        {
+            _transaction.Rollback();
+        }
+
+        private void UpdateEntityState<T>(T entity, EntityState state) where T : BaseEntity
+        {
+            var dbEntityEntry = GetDbEntry(entity);
+            dbEntityEntry.State = state;
+        }
+
+        private DbEntityEntry GetDbEntry<T>(T entity) where T : BaseEntity
+        {
+            var dbEntityEntry = Entry(entity);
+            if (dbEntityEntry.State == EntityState.Detached)
+                Set<T>().Attach(entity);
+
+            return dbEntityEntry;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_objectContext != null && _objectContext.Connection.State == ConnectionState.Open)
+                    _objectContext.Connection.Close();
+                if (_objectContext != null)
+                    _objectContext.Dispose();
+                if (_transaction != null)
+                    _transaction.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ src/DotNetCore.Data.EntityFramework/BaseUnitOfWork.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DotNetCore.Core.Data;
+using DotNetCore.Core.Utilities;
+
+namespace DotNetCore.Data.EntityFramework
+{
+    public abstract class BaseUnitOfWork : IUnitOfWork
+    {
+        private readonly IDbContext _context;
+        private bool _disposed;
+
+        protected BaseUnitOfWork(IDbContext context)
+        {
+            context.ThrowIfNull("context");
+
+            _context = context;
+            _disposed = false;
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public void BeginTransaction()
+        {
+            _context.BeginTransaction();
+        }
+
+        public int Commit()
+        {
+            return _context.Commit();
+        }
+
+        public void Rollback()
+        {
+            _context.Rollback();
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.CommitAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                _context.Dispose();
+            }
+            _disposed = true;
+        }
+    }
+}
