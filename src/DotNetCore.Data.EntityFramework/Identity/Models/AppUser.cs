e3db0cdff46162014d3251e325fddc872d362ef3
Neil Smith <nsmith@envoc.com>
2015-01-15 19:27:52 -0600

    feat(data.entityframework.security): create identity models and dbcontext

---
+++ src/DotNetCore.Data.EntityFramework/Identity/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DotNetCore.Data.EntityFramework.Identity.Models
+{
+    public class AppUser<T> : IdentityUser<T, AppUserLogin<T>, AppUserRole<T>, AppUserClaim<T>>
+    {
+    }
+}
