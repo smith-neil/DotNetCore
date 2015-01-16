16d74def390855e857843cc39308846e0114ab00
Neil Smith <nsmith@envoc.com>
2015-01-15 20:53:15 -0600

    feat(infrastructure.logging): create nlog logger

---
+++ src/DotNetCore.Infrastructure/Logging/NLogLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DotNetCore.Core.Utilities;
+using NLog;
+
+namespace DotNetCore.Infrastructure.Logging
+{
+    public class NLogLogger : ILogger
+    {
+        private static readonly Lazy<NLogLogger> LazyLogger = new Lazy<NLogLogger>(() => new NLogLogger()); 
+        private static readonly Lazy<NLog.Logger> LazyNLogger= new Lazy<Logger>(LogManager.GetCurrentClassLogger);
+
+        private NLogLogger() { }
+
+        public static ILogger Instance
+        {
+            get { return LazyLogger.Value; }
+        }
+
+        public void Log(string message)
+        {
+            LazyNLogger.Value.Info(message);
+        }
+
+        public void Log(Exception exception)
+        {
+            LazyNLogger.Value.Error(exception);
+        }
+    }
+}
