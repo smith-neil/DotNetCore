91e00054327097eaee68ec2414efa2aba6a15623
Neil Smith <nsmith@envoc.com>
2015-02-09 17:54:16 -0600

    feat(data.entityframework): move fluent repositories to fluent namespace

---
+++ src/DotNetCore.Core/Data/IReadOnlyRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCore.Core.Domain;
+
+namespace DotNetCore.Core.Data
+{
+    public interface IReadOnlyRepository<TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> AsQueryable();
+
+        TEntity GetById(int id);
+        Task<TEntity> GetByIdAsync(int id);
+        
+        IEnumerable<TEntity> GetAll();
+        Task<IEnumerable<TEntity>> GetAllAsync();
+    }
+}
---
+++ src/DotNetCore.Core/Data/IRepositroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using DotNetCore.Core.Domain;
+
+namespace DotNetCore.Core.Data
+{
+    public interface IRepositroy<TEntity> : IReadOnlyRepository<TEntity> where TEntity : BaseEntity
+    {
+        void Add(TEntity entity);
+
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Update(TEntity entity);
+
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
